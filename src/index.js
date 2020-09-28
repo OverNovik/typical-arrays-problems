5bc7efe18574ac7cda21282690606657952c2fc9
Aliaxander Novikov <nerevarin11@gmail.com>
2020-09-28 21:33:43 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+  let minNumber = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] < minNumber) {
+            minNumber = array[i];
+        } 
+    }
+    return minNumber;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+  let minNumber = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] > minNumber) {
+            minNumber = array[i];
+        } 
+    }
+    return minNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+  var sum = 0;
+   for (var i = 0; i < array.length; i++) {
+       sum += array[i];
+   }
+   return sum / array.length;
 }
